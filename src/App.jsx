dba81ba01eadaa3bed60edf3f6ef6cf287724ff8
Eric (Mansa) Marah <devopsmansa@gmail.com>
2024-03-17 19:21:56 -0500

    Fifth commit: Added a search bar on the nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react"; 
+import "./App.css"; 
+import search from '../src/assets/icons/search.svg'; 
 
 function App() {
- 
+  const [input, setinput] = useState(''); // State for input field
 
   return (
-    <div className='text-xl font-bold text-center'>
-      hello fam
+  
+    <div className="w-full h-screen text-white px-8">
+      {/* Navigation bar */}
+      <nav className="w-full p-3 flex justify-between items-center">
+  
+        <h1 className="font-bold tracking-wide text-3xl">Weather App</h1>
+ 
+        <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2">
+          {/* Search icon */}
+          <img src="{search}" alt="search" className="w-[1.5rem] h-[1.5rem]" />
+          {/* Input field */}
+          <input
+          
+            onKeyUp={(e) => {
+              if (e.key === 'Enter') {
+              
+              }
+            }}
+            type="text" 
+            className="focus:outline-none w-full text-[#212121] text-lg" // Styling for input field
+            value={input} 
+            onChange={ e => setinput(e.target.value)} // Handling input change
+          />
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
